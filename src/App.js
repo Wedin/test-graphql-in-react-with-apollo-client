160917dcbb7fac1fe9b3deb174bccd4bc871ddf6
Daniel Wedin <danielwedin@hotmail.se>
2019-08-01 16:04:32 +0200

    Add client side star

--- src/App.js
+++ src/App.js
@@ -5,8 +5,48 @@ import { ApolloProvider } from "react-apollo";
 import RecipesList from "./components/RecipesList";
 import AddRecipe from "./components/AddRecipe";
 
+const starredRecipesStorage = {
+  STORAGE_KEY: "starredRecipes",
+  getAll: () => {
+    const rawItems = localStorage.getItem(starredRecipesStorage.STORAGE_KEY) || [];
+    return JSON.parse(rawItems);
+  },
+  set: starredRecipes => {
+    localStorage.setItem(starredRecipesStorage.STORAGE_KEY, JSON.stringify(starredRecipes));
+  },
+};
+
+const resolvers = {
+  Recipe: {
+    isStarred: parent => {
+      const starredRecipes = starredRecipesStorage.getAll();
+      return starredRecipes.includes(parent.id);
+    },
+  },
+  Mutation: {
+    updateRecipeStarred: (_, variables) => {
+      const starredRecipes = starredRecipesStorage.getAll();
+      if (variables.isStarred) {
+        starredRecipes.push(variables.id);
+        starredRecipesStorage.set(starredRecipes);
+      } else {
+        const filteredRecipes = starredRecipes.filter(recipeId => recipeId !== variables.id);
+        starredRecipesStorage.set(filteredRecipes);
+      }
+
+      return {
+        __typename: "Recipe",
+        isStarred: variables.isStarred,
+      };
+    },
+  },
+};
+
 const client = new ApolloClient({
   uri: "http://localhost:4000",
+  clientState: {
+    resolvers: resolvers,
+  },
 });
 
 function App() {
--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -1,14 +1,62 @@
 import React, { useState } from "react";
-import { Query } from "react-apollo";
+import { Query, Mutation } from "react-apollo";
+import gql from "graphql-tag";
 import { recipesQuery } from "../queries";
+
+const updateRecipeStarredMutation = gql`
+  mutation updateRecipeStarred($id: ID!, $isStarred: Boolean!) {
+    updateRecipeStarred(id: $id, isStarred: $isStarred) @client
+  }
+`;
 
 const RecipesList = ({ recipes }) => {
   return (
     <>
       <h3>Recipes:</h3>
       <ul>
-        {recipes.map(({ id, title }) => (
-          <li key={id}>{title}</li>
+        {recipes.map(({ id, title, isStarred }) => (
+          <li key={id}>
+            {title}
+            <Mutation
+              mutation={updateRecipeStarredMutation}
+              refetchQueries={[
+                {
+                  query: recipesQuery,
+                  variables: { vegetarian: true },
+                },
+                {
+                  query: recipesQuery,
+                  variables: { vegetarian: false },
+                },
+              ]}
+              awaitRefetchQueries={true}
+            >
+              {(updateRecipeStarred, { loading, error }) => {
+                return (
+                  <button
+                    type="button"
+                    onClick={() => {
+                      updateRecipeStarred({
+                        variables: {
+                          id,
+                          isStarred: !isStarred,
+                        },
+                      });
+                    }}
+                  >
+                    <span
+                      style={{ filter: isStarred ? "" : "grayscale(1)" }}
+                      role="img"
+                      aria-label={isStarred ? "starred" : "not starred"}
+                    >
+                      ⭐️
+                    </span>
+                    {error && "Failed up update"}
+                  </button>
+                );
+              }}
+            </Mutation>
+          </li>
         ))}
       </ul>
     </>
@@ -31,6 +79,7 @@ export default () => {
             return <p>Loading...</p>;
           }
           if (error) {
+            console.log("error: ", error);
             return <p>Something went wrong</p>;
           }
           return <RecipesList recipes={data.recipes} />;
