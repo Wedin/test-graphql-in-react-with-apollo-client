13f69a8e540d7ca48cd07a78a63cf8c177c9ae14
Daniel Wedin <danielwedin@hotmail.se>
2019-08-01 14:38:30 +0200

    Set up cra and server

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+import "./App.css";
+import { ApolloProvider, ApolloConsumer } from "react-apollo";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ApolloProvider client={client}>
+        <ApolloConsumer>
+          {client => {
+            client
+              .query({
+                query: gql`
+                  {
+                    recipes {
+                      id
+                      title
+                    }
+                  }
+                `,
+              })
+              .then(res => console.log(res));
+            return null;
+          }}
+        </ApolloConsumer>
+      </ApolloProvider>
     </div>
   );
 }
